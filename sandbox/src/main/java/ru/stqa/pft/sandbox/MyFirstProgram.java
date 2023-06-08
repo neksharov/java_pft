d875cc6f9c321a37f9453b264ea627dab4392cf6
Nek <65871700+neksharov@users.noreply.github.com>
2023-06-09 01:16:17 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,12 +2,32 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, "+"world!"); //результат конкатенации - отдельное значение, состоящее из двух значений (слов)/литеральное значение - строка/
+		 hello("world");
+		 hello("user");//результат конкатенации - отдельное значение, состоящее из двух значений (слов)/литеральное значение - строка/
 		// число, вычислимое значение - результат какого-либо выражения
 
 
-		int l =6;
-		int s = l*l;
-		System.out.println("Площадь квадрата со стороной "+l+"="+s);
+		/*double l =6.0;
+		double s = l*l;
+		System.out.println("Площадь квадрата со стороной "+l+"="+s);*/
+
+		double len = 5;
+		System.out.println("Плозадь квадрата со стороной " + len + "=" + area(len));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + "=" + area(a, b));
+	}
+	//описание функции; String somebody - параметр функции; void - ничего не возвращает
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody);
+	}
+	//область видимости переменной - фигурные скобки, в которых она определена (double l)
+	public static double area(double l) {
+		return l*l;
+	}
+
+	public static double area(double a, double b) {
+		return a*b;
 	}
 }
