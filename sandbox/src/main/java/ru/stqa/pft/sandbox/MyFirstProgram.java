74d9796e9d1391292239840b2658afd0a1bff1d3
Nek <65871700+neksharov@users.noreply.github.com>
2023-06-09 00:50:39 +0300

    Добавлены определения значений и пример работы выражения с переменными

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, "+"world!"); //результат конкатенации - отдельное значение, состоящее из двух значений (слов)/литеральное значение - строка/
+		// число, вычислимое значение - результат какого-либо выражения
+
+
+		int l =6;
+		int s = l*l;
+		System.out.println("Площадь квадрата со стороной "+l+"="+s);
 	}
 }
