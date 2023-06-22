fc077a70a634ba7d8f567eb538d5cf18b8c991cd
Nek <65871700+neksharov@users.noreply.github.com>
2023-06-22 19:21:39 +0300

    добавлены тесты для класса Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Pointdata.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Pointdata.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class pointData {
     public static void main(String[] args) {
-        Point p1 = new Point(2, 6);
-        Point p2 = new Point(0, 9);
+        Point p1 = new Point(-1, -8);
+        Point p2 = new Point(-5, -3);
         System.out.println("Расстояние между точками = Точка 1 (" + p1.x + ";" + p1.y + ") + Точка 2 (" + p2.x + ";" + p2.y + ")" + " = " + p1.distance(p2));
 
     }
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+    @Test
+    //разные значения
+    public void testDistance(){
+        Point p1 = new Point(2, 6);
+        Point p2 = new Point(7, 9);
+        Assert.assertEquals(p1.distance(p2), 5.830951894845301);
+    }
+    //одинковые значения
+    public void testSameData(){
+        Point p1 = new Point(3, 3);
+        Point p2 = new Point(3, 3);
+        Assert.assertEquals(p1.distance(p2), 0);
+    }
+    //отрицательные значения
+    public void testNegativeData(){
+        Point p1 = new Point(-1, -8);
+        Point p2 = new Point(-5, -3);
+        Assert.assertEquals(p1.distance(p2), 6.4031242374328485);
+    }
+}
