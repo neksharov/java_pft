06ddc20969c63f39fa32fd658c7942fdad057d58
Nek <65871700+neksharov@users.noreply.github.com>
2023-06-23 11:22:45 +0300

    обновил задание 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -12,12 +12,14 @@ public class PointTests {
         Assert.assertEquals(p1.distance(p2), 5.830951894845301);
     }
     //одинковые значения
+    @Test
     public void testSameData(){
         Point p1 = new Point(3, 3);
         Point p2 = new Point(3, 3);
         Assert.assertEquals(p1.distance(p2), 0);
     }
     //отрицательные значения
+    @Test
     public void testNegativeData(){
         Point p1 = new Point(-1, -8);
         Point p2 = new Point(-5, -3);
