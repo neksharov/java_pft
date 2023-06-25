f95a598c3167a22ec576203480e3edab3977b7bc
Nek <65871700+neksharov@users.noreply.github.com>
2023-06-25 19:10:02 +0300

    добавлено задание 4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,104 @@
+package ru.stqa.pft.addressbook;
+
+import java.time.Duration;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class ContactCreationTests {
+  private WebDriver wd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    wd.get("http://localhost/addressbook/index.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    initContactCreation();
+    fillContactForm(new ContactData("Nick", "Alex", "Sharov", "testnick", "test", "beeline", "mira avenu 33-111", "+79991230000", "+79996660011", "+79991116666", "test@gmail.com", "test2@gmail.com"));
+    submitContactCreation();
+    goToHomePage();
+    returnToHomePage();
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void goToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.name());
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(contactData.middlename());
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.lastname());
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactData.nickname());
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.title());
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.company());
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.address());
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.homephone());
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.mobilephone());
+    wd.findElement(By.name("work")).clear();
+    wd.findElement(By.name("work")).sendKeys(contactData.workphone());
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.email1());
+    wd.findElement(By.name("email2")).clear();
+    wd.findElement(By.name("email2")).sendKeys(contactData.email2());
+  }
+
+  private void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,125 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.Objects;
+
+public final class ContactData {
+    private final String name;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String homephone;
+    private final String mobilephone;
+    private final String workphone;
+    private final String email1;
+    private final String email2;
+
+    public ContactData(String name, String middlename, String lastname, String nickname, String title,
+                       String company, String address, String homephone, String mobilephone, String workphone,
+                       String email1, String email2) {
+        this.name = name;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.homephone = homephone;
+        this.mobilephone = mobilephone;
+        this.workphone = workphone;
+        this.email1 = email1;
+        this.email2 = email2;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String middlename() {
+        return middlename;
+    }
+
+    public String lastname() {
+        return lastname;
+    }
+
+    public String nickname() {
+        return nickname;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String company() {
+        return company;
+    }
+
+    public String address() {
+        return address;
+    }
+
+    public String homephone() {
+        return homephone;
+    }
+
+    public String mobilephone() {
+        return mobilephone;
+    }
+
+    public String workphone() {
+        return workphone;
+    }
+
+    public String email1() {
+        return email1;
+    }
+
+    public String email2() {
+        return email2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ContactData) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.middlename, that.middlename) &&
+                Objects.equals(this.lastname, that.lastname) &&
+                Objects.equals(this.nickname, that.nickname) &&
+                Objects.equals(this.title, that.title) &&
+                Objects.equals(this.company, that.company) &&
+                Objects.equals(this.address, that.address) &&
+                Objects.equals(this.homephone, that.homephone) &&
+                Objects.equals(this.mobilephone, that.mobilephone) &&
+                Objects.equals(this.workphone, that.workphone) &&
+                Objects.equals(this.email1, that.email1) &&
+                Objects.equals(this.email2, that.email2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, middlename, lastname, nickname, title, company, address, homephone, mobilephone, workphone, email1, email2);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData[" +
+                "name=" + name + ", " +
+                "middlename=" + middlename + ", " +
+                "lastname=" + lastname + ", " +
+                "nickname=" + nickname + ", " +
+                "title=" + title + ", " +
+                "company=" + company + ", " +
+                "address=" + address + ", " +
+                "homephone=" + homephone + ", " +
+                "mobilephone=" + mobilephone + ", " +
+                "workphone=" + workphone + ", " +
+                "email1=" + email1 + ", " +
+                "email2=" + email2 + ']';
+    }
+
+}
